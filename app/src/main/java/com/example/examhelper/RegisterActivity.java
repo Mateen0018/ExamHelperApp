370c4ca44d7b67d2407beac6d797ddfee6f5b1e2
Mateen <mxs6497@psu.edu>
2022-11-08 14:56:02 -0500

    added new files

--- app/src/main/java/com/example/examhelper/RegisterActivity.java
+++ app/src/main/java/com/example/examhelper/RegisterActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
 
         TextView btn=findViewById(R.id.alreadyHaveAccount);
         btn.setOnClickListener(new View.OnClickListener() {
